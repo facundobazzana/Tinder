f2bd2ceeff93a6304ff20d694ee9501a0772bc55
Facundo Bazzana <facundo.bazzana@widergy.com>
2016-12-11 18:22:01 -0300

    Se agrega eslint

--- index.ios.js
+++ index.ios.js
@@ -7,6 +7,6 @@
 'use strict';
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-
 import App from './App/container/';
+
 AppRegistry.registerComponent('Tinder', () => App);
